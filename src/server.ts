d33f20401b48704cdecbc4c295640eed299324a5
Guilhermo Masid <guilhermomasid@gmail.com>
2020-12-01 17:17:18 -0300

    some adjusts

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => response.json({ guilhermo: 'lindo' }));
+app.get('/', (request, response) => {
+  return response.json({ guilhermo: 'lindo' });
+});
 
 app.listen(3333, () => {
   console.log('🧔 GoBarber server started on port 3333!');
