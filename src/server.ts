111a9552f924811df89e2437085b978e0c2edd15
Guilhermo Masid <guilhermomasid@gmail.com>
2020-12-01 16:41:02 -0300

    removing node_modules

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,9 @@ import express from 'express';
 const app = express();
 
 app.get('/', (request, response) => {
-  return response.json({ guilhermo: 'lind' })
+  return response.json({ guilhermo: 'lindo' })
 })
 
 app.listen(3333, () => {
-  console.log('🧔🏻 GoBarber server started on port 3333!')
+  console.log('🧔 GoBarber server started on port 3333!')
 })
