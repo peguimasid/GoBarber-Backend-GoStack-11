a4367ea0d2a1554fadf19a1a76b1a539967a0948
Guilhermo Masid <guilhermomasid@gmail.com>
2020-12-01 18:39:44 -0300

    adding configs to user VSCode debugger

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,16 @@
 import { Router } from 'express';
 
 const routes = Router();
+
+routes.post('/', (request, response) => {
+  const { email, password } = request.body;
+
+  const user = {
+    email,
+    password,
+  };
+
+  return response.json(user);
+});
+
 export default routes;
--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,9 @@ import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ guilhermo: 'lindo' });
-});
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('🧔 GoBarber server started on port 3333!');
