d63fa631da50fd4b4517a8290338c3fddf2b5d98
Guilhermo Masid <guilhermomasid@gmail.com>
2020-12-30 18:46:34 -0300

    criando tabela de agendamentos

---
+++ src/database/migrations/1609361712593-CreateAppointments.ts
@@ -0,0 +1,36 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateAppointments1609361712593
+  implements MigrationInterface {
+  protected tableName = 'appointments';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: this.tableName,
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'provider',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'date',
+            type: 'timestamp with time zone',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable(this.tableName);
+  }
+}
