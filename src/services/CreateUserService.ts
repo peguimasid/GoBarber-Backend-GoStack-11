fe3a9fbc25de9189241d6d84c07ab44a9256ade3
Guilhermo Masid <guilhermomasid@gmail.com>
2021-01-06 18:13:20 -0300

    criptografando senha

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,4 +1,6 @@
 import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+
 import User from '../models/User';
 
 interface Request {
@@ -6,7 +8,6 @@ interface Request {
   email: string;
   password: string;
 }
-
 class CreateUserService {
   public async execute({ name, email, password }: Request): Promise<User> {
     const userRepository = getRepository(User);
@@ -17,10 +18,12 @@ class CreateUserService {
 
     if (userExists) throw Error('Email address already used');
 
+    const hashedPassword = await hash(password, 8);
+
     const user = userRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     await userRepository.save(user);
