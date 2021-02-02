e5aabc78a69364efe61b8a963b4a1791ac1f6291
Guilhermo Masid <guilhermomasid@gmail.com>
2021-02-02 18:05:14 -0300

    Autenticacao: Rotas autenticadas

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '158fa39f82db171f9f941f610488d98f',
+    expiresIn: '1d',
+  },
+};
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
+import authConfig from '../config/auth';
 
 import User from '../models/User';
 
@@ -30,9 +31,11 @@ class AuthenticationUserService {
       throw new Error('Incorrect email/password combination');
     }
 
-    const token = sign({}, '158fa39f82db171f9f941f610488d98f', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return {
